3c17e34ded207aa48810ea910f23c633cb2f54cb
Muhammad  Hussain <zainjahangir4567@gmail.com>
2024-06-30 04:29:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//1...COMPUTER WILL GENERATE A RANDOM NUMBER
+//2...USER INPUT GUSSEING NUMBER
+//3...COMPARE USER INPUT WITH COMPUTER GUSSESING NUMBER
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "useraGussesingNumber",
+        type: "number",
+        message: "plz guest a number 1 to 10"
+    }
+]);
+console.log(answers);
+if (answers.useraGussesingNumber === randomNumber) {
+    console.log("CONGRATUALATION  IS GUSSEING RIGHR NUMBER");
+}
+else {
+    console.log("your guest wrong number");
+}
